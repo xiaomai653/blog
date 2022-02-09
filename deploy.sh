36d7997f1ae3b0680d1ead2ff37df7ca350ca776
xiaomai <xiaomai653@163.com>
2022-02-09 11:44:37 +0800

    修改自动部署

--- deploy.sh
+++ deploy.sh
@@ -12,18 +12,22 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
+# 设置github路径
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:xiaomai653/xiaomai653.github.io.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://${GITHUB_TOKEN}@github.com/xiaomai653/xiaomai653.github.io.git
+  git config --global user.name "xiaomai"
+  git config --global user.email "1271899086@qq.com"
+fi
+
+# 上传代码
 git init
 git add -A
-git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-# 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-# git push -f https://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@github.com/xiaomai653/blog.git master:gh-pages
-git push -f https://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@github.com/xiaomai653/xiaomai653.github.io.git master:gh-pages
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
 
 cd -
+rm -rf docs/.vuepress/dist
